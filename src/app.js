3d82d26226146dfae207b76ceaacbb70ab4c3549
Harshvardhan2325 <165410973+Harshvardhan2325@users.noreply.github.com>
2025-02-12 12:27:07 +0000

    image path change

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (reg, res) => {
     res.sendFile(path.join(__dirname, 'pages/index.html'));
